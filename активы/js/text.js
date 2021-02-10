fe7089e3547fd722a5b4af948167da0a9b8ec5f0
SmokName <77127685+SmokName@users.noreply.github.com>
2021-02-10 11:17:57 +0500

    Update and rename assets/js/text.js to активы/js/text.js

--- активы/js/text.js
+++ активы/js/text.js
@@ -20,7 +20,7 @@ $(function () {
 });
 $(function () {
     $(".typed").typed({
-        strings: ['Smoke', "Batch file", "Python developer", "bruh"],
+        strings: ['Smoke', "Batch file", "Python developer", "bruh", "Hentai",],
         typeSpeed: 100,
         loop: true,
         cursorChar: "|",
